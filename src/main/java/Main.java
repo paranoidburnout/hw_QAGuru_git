bf6f7056a502b583449fc36608c4eba9391eb83a
Elizaveta Vasenko <57870900+paranoidburnout@users.noreply.github.com>
2023-02-15 14:21:10 +0300

    Update Main.java
    add new func

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
+private static String userAccount = "QA_GURU";
+private static String message = "Hello" 
+
     public static void main(String[] args){
-        System.out.println("Hello!");
+        System.out.println(message + userAccount);
     }
 }
