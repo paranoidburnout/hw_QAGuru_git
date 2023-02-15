6a5220bc0b481199387751861cac64edbf0097bb
Elizaveta Vasenko <57870900+paranoidburnout@users.noreply.github.com>
2023-02-15 14:09:43 +0300

    Update Main.java
    unused fields were removed, necessary ones were created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 public class Main {
+   
+public static String msg = "You can do it!";
 
-    private static int number = 2;
-    private static final String name = "WORLD!";
-    private static String message = "PLS DON'T FORGET: delete this code";
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println(msg);
     }
 }
