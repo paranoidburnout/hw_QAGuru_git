70e6259ac5643ad36674f8d5042b715aabba446c
Lissa Vasenko <lisa.vasenko@outlook.com>
2023-02-15 14:02:49 +0300

    new ingenious fields for flexible work with code!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
+    private static int number = 2;
+    private static final String name = "WORLD!";
+    private static String message = "PLS DON'T FORGET: delete this code";
     public static void main(String[] args){
-        System.out.println("Hello!");
+        System.out.println("Hello world!");
     }
 }
